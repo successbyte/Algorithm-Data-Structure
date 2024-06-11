881a927ad5912ba70fd1d8efedf92bb74733eb62
Ehsanullah Haidary <ehsanullahhaidary@gmail.com>
2024-06-11 11:33:54 +0430

    Pop method was created and Done

--- exersices/LinkedList.js
+++ exersices/LinkedList.js
@@ -29,8 +29,51 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
+
+    // if (!this.head) {
+    //     return undefined;
+    //   }
+    // else if (this.length === 1) {
+    //   this.temp = this.head;
+    //   this.head = null;
+    //   this.tail = null;
+    //   this.length = 0;
+    //   return this.temp;
+    // } else {
+    //   this.temp = this.head;
+    //   for (let node = 1; node < this.length; node++) {
+    //     if (node !== this.length - 1) {
+    //       this.temp = this.temp.next;
+    //     } else if (node === this.length - 1) {
+    //       this.tail = this.temp;
+    //       this.temp.next = null;
+    //       this.length--;
+    //     }
+    //   }
+    // }
+  }
 }
 
-const myLL = new LinkedList(7);
-myLL.push(4);
+const myLL = new LinkedList(1);
+myLL.push(2);
+
 console.log(myLL);
