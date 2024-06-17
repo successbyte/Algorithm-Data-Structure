4618da7641d6f4cb44b4e54917ac39642001a6cc
Ehsanullah Haidary <ehsanullahhaidary@gmail.com>
2024-06-17 17:42:13 +0430

    Pop method was created and Done

--- exersices/DoublyLinkList.js
+++ exersices/DoublyLinkList.js
@@ -27,7 +27,26 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let temp = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+    this.length--;
+    return temp;
+  }
 }
 
 let doubleLinkedList = new DoublyLinkedList(5);
+doubleLinkedList.push(10);
+
 console.log(doubleLinkedList);
