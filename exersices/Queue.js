6acff065a64185be18a8e2bc2eba1ca225071677
Ehsanullah Haidary <ehsanullahhaidary@gmail.com>
2024-06-21 16:27:44 +0430

    Enqueue method in Queue was created and Done

--- exersices/Queue.js
+++ exersices/Queue.js
@@ -12,6 +12,19 @@ class Queue {
     this.last = newNode;
     this.length = 1;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let myQueue = new Queue(10);
