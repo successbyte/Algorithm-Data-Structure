b08798009abb1a54607e41570e988c6b332f123f
Ehsanullah Haidary <ehsanullahhaidary@gmail.com>
2024-08-15 18:46:15 +0430

    BST insert method done

--- exersices/BST.js
+++ exersices/BST.js
@@ -10,7 +10,40 @@ class BST {
   constructor(value) {
     this.root = null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this.root;
+    }
+
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
 }
 
-const newBST = new BST(2);
-console.log(newBST);
+const myBST = new BST(2);
+myBST.insert(47);
+myBST.insert(21);
+myBST.insert(76);
+myBST.insert(18);
+myBST.insert(52);
+myBST.insert(82);
+
+console.log(myBST);
