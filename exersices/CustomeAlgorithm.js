ed43362cf5dc36d9e9dad6f6c2788f460aa32d2a
Ehsanullah Haidary <ehsanullahhaidary@gmail.com>
2024-10-03 11:51:10 +0430

    Feat: Custom algo added it returns true or false if they have any item in common or not

--- exersices/CustomeAlgorithm.js
+++ exersices/CustomeAlgorithm.js
@@ -1,3 +1,4 @@
+// ## 1
 // printing bellow template
 //   X X X X X
 //     X X X X
@@ -15,5 +16,25 @@ function print(iteration) {
     }
   }
 }
-
 // print(5);
+
+// ## 2
+// bellow function returns true if the two arrays has an item in common
+// and returns false if they don't have any item in common
+
+function itemInCommon(arr1, arr2) {
+  let obj = {};
+
+  for (let index = 0; index < arr1.length; index++) {
+    obj[arr1[index]] = true;
+  }
+
+  for (const element of arr2) {
+    if (obj[element]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(itemInCommon([1, 3, 5], [2, 4, 5]));
