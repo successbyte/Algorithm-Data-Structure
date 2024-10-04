87b495547c113c78bed32b6f6604fa92f60f6288
Ehsanullah Haidary <ehsanullahhaidary@gmail.com>
2024-10-04 11:21:29 +0430

    Feat: Graph addVertex method added

--- exersices/Graph.js
+++ exersices/Graph.js
@@ -2,6 +2,14 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+      return true;
+    }
+    return false;
+  }
 }
 
 let myGraph = new Graph();
