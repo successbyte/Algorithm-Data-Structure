ebf9e1f0f55bc4fdc3c362d6cc992166d47b33a5
Ehsanullah Haidary <ehsanullahhaidary@gmail.com>
2024-10-06 08:53:04 +0430

    Feat: insertionSort added

--- exersices/BasicSorts.js
+++ exersices/BasicSorts.js
@@ -29,4 +29,16 @@ function selectionSort(array) {
   return array;
 }
 
-console.log(selectionSort([4, 2, 6, 5, 3, 1]));
+function insertionSort(array) {
+  let temp;
+  for (let i = 1; i < array.length; i++) {
+    temp = array[i];
+    for (var j = i - 1; array[j] > temp && j > -1; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = temp;
+  }
+  return array;
+}
+
+console.log(insertionSort([4, 2, 6, 5, 3, 1]));
