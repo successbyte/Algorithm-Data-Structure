bfb8e69222d3e635b08e448ee65a5286bc365248
Ehsanullah Haidary <ehsanullahhaidary@gmail.com>
2024-10-06 10:26:00 +0430

    Feat: Merge method added

---
+++ exersices/MergeSort.js
@@ -0,0 +1,25 @@
+function Merge(array1, array2) {
+  let combined = [];
+  let i = 0;
+  let j = 0;
+  while (i < array1.length && j < array2.length) {
+    if (array1[i] < array2[j]) {
+      combined.push(array1[i]);
+      i++;
+    } else {
+      combined.push(array2[j]);
+      j++;
+    }
+  }
+  while (i < array1.length) {
+    combined.push(array1[i]);
+    i++;
+  }
+  while (j < array2.length) {
+    combined.push(array1[j]);
+    i++;
+  }
+  return combined;
+}
+
+console.log(Merge([1, 3, 7, 8], [2, 4, 5, 6]));
